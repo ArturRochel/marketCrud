5d9e4c226958fa20e1869c35c290f11d89dddde9
arturrochel <arturrochel@gmail.com>
2025-08-07 11:28:56 -0300

    Alteração rota push e delete
    Foi alterado a rota POST e DELETE para um modelo de try catch e funções assíncronas. Além disso, foram modificados o acesso ao .env

--- back/index.js
+++ back/index.js
@@ -1,28 +1,28 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const routes = require("./routes/produtos.js")
-require("dotenv").config
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes/produtos.js");
 
-const PORT = process.env.PORT || 3001
-const app = express()
+const PORT = process.env.PORT || 3001;
+const app = express();
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+const mongoUri = process.env.MONGODB_URI;
 
-const mongoUri = process.env.MONGODB_URI
-
-mongoose.connect(mongoUri)
+mongoose
+  .connect(mongoUri)
   .then(() => {
-    console.log("Conexão com o MongoDB Atlas estabelecida com sucesso.")
+    console.log("Conexão com o MongoDB Atlas estabelecida com sucesso.");
     app.listen(PORT, () => {
-     console.log(`server on: http://localhost:${PORT}`)
-    })
+      console.log(`server on: http://localhost:${PORT}`);
+    });
   })
   .catch((error) => {
-    console.log("Erro ao conectar ao MondoDB Atlas")
-    process.exit(1)
-  })
+    console.log(`${error}`);
+    process.exit(1);
+  });
 
-app.use("/api/produtos", routes)
+app.use("/api/produtos", routes);
--- back/routes/produtos.js
+++ back/routes/produtos.js
@@ -1,86 +1,96 @@
 const { Router } = require("express");
+const Produto = require("../models/Produto.js");
 const routes = Router();
 
-let dbProdutos = []; 
+let dbProdutos = [];
 
 routes.get("/", (request, response) => {
-    return response.status(200).json("API de Produtos está online!");
+  return response.status(200).json("API de Produtos está online!");
 });
 
 routes.post("/", (request, response) => {
-    const { nome, precoCompra, precoVenda, descricao } = request.body;
-    const objetoData = new Date();
-    const data = objetoData.toLocaleDateString("pt-BR");
-    const hora = objetoData.toLocaleTimeString("pt-BR");
+  const { nome, precoCompra, precoVenda, descricao } = request.body;
+  const objetoData = new Date();
+  const data = objetoData.toLocaleDateString("pt-BR");
+  const hora = objetoData.toLocaleTimeString("pt-BR");
 
-    const novoProduto = {
-        id: dbProdutos.length ? dbProdutos[dbProdutos.length - 1].id + 1 : 1,
-        nome,
-        precoCompra,
-        precoVenda,
-        descricao,
-        dataAtualizacao: `${data} ${hora}`
-    };
-    
-    dbProdutos.push(novoProduto);
+  const novoProduto = {
+    id: dbProdutos.length ? dbProdutos[dbProdutos.length - 1].id + 1 : 1,
+    nome,
+    precoCompra,
+    precoVenda,
+    descricao,
+    dataAtualizacao: `${data} ${hora}`,
+  };
 
-    return response.status(201).json(novoProduto);
+  dbProdutos.push(novoProduto);
+
+  return response.status(201).json(novoProduto);
 });
-
 
 routes.get("/", (request, response) => {
-    return response.status(200).json(dbProdutos);
+  return response.status(200).json(dbProdutos);
 });
 
+routes.get("/busca", async (request, response) => {
+  try {
+    const { nome } = request.query;
+    const produtosEncontrados = await Produto.find({
+      nome: { $regex: nome, $options: "i" },
+    });
 
-routes.get("/:id", (request, response) => {
-    const idBusca = request.params.id;
-    const produtoEncontrado = dbProdutos.find((produto) => produto.id == idBusca);
-
-    if (produtoEncontrado) {
-        return response.status(200).json(produtoEncontrado);
+    if (produtosEncontrados.length === 0) {
+      return response
+        .status(404)
+        .json({ message: "Nenhum produto encontrado." });
     } else {
-        return response.status(404).json("Produto não encontrado!");
+      return response.status(200).json(produtosEncontrados);
     }
+  } catch (error) {
+    return response.status(500).json({ error: "Ocorreu um erro na busca" });
+  }
 });
 
 routes.put("/:id", (request, response) => {
-    const { nome, precoCompra, precoVenda, descricao } = request.body;
-    const id = request.params.id;
-    const indexProduto = dbProdutos.findIndex(produto => produto.id == id);
-    const objetoData = new Date();
-    const data = objetoData.toLocaleDateString("pt-BR");
-    const hora = objetoData.toLocaleTimeString("pt-BR");
+  const { nome, precoCompra, precoVenda, descricao } = request.body;
+  const id = request.params.id;
+  const indexProduto = dbProdutos.findIndex((produto) => produto.id == id);
+  const objetoData = new Date();
+  const data = objetoData.toLocaleDateString("pt-BR");
+  const hora = objetoData.toLocaleTimeString("pt-BR");
 
-    if (indexProduto !== -1) {
-        const atualizacao = {
-            ...dbProdutos[indexProduto], 
-            id: Number(id),
-            nome: nome || dbProdutos[indexProduto].nome, 
-            precoCompra: precoCompra || dbProdutos[indexProduto].precoCompra,
-            precoVenda: precoVenda || dbProdutos[indexProduto].precoVenda,
-            descricao: descricao || dbProdutos[indexProduto].descricao,
-            dataAtualizacao: `${data} ${hora}`
-        };
-        dbProdutos[indexProduto] = atualizacao;
-        return response.status(200).json(atualizacao);
-    } else {
-        return response.status(404).json("Produto não encontrado!");
-    }
+  if (indexProduto !== -1) {
+    const atualizacao = {
+      ...dbProdutos[indexProduto],
+      id: Number(id),
+      nome: nome || dbProdutos[indexProduto].nome,
+      precoCompra: precoCompra || dbProdutos[indexProduto].precoCompra,
+      precoVenda: precoVenda || dbProdutos[indexProduto].precoVenda,
+      descricao: descricao || dbProdutos[indexProduto].descricao,
+      dataAtualizacao: `${data} ${hora}`,
+    };
+    dbProdutos[indexProduto] = atualizacao;
+    return response.status(200).json(atualizacao);
+  } else {
+    return response.status(404).json("Produto não encontrado!");
+  }
 });
 
+routes.delete("/excluir", async (request, response) => {
+  try {
+    const { nome } = request.query;
+    const produtoExcluido = await Produto.find({
+      nome: { $regex: "^" + nome + "$", $options: "i" },
+    });
 
-routes.delete("/:id", (request, response) => {
-    const idDelete = request.params.id;
-    const indexExclusao = dbProdutos.findIndex((produto) => produto.id == idDelete);
-
-    if (indexExclusao !== -1) {
-        const produtoExcluido = dbProdutos[indexExclusao]; 
-        dbProdutos.splice(indexExclusao, 1);
-        return response.status(200).json(produtoExcluido); 
+    if (!produtoExcluido) {
+      return response.status(404).json({ message: "Produto não encontrado" });
     } else {
-        return response.status(404).json("Produto não encontrado");
+      return response.status(201).json(produtoExcluido);
     }
+  } catch (error) {
+    response.status(500).json({ message: "Erro na exclusão do produto" });
+  }
 });
 
 module.exports = routes;
